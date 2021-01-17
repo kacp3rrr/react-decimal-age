1f9e06856fdf006da0b94e9f9615f4e0d7256bf7
Kacp3rr <64568015+Kacp3rr@users.noreply.github.com>
2021-01-17 16:21:48 -0500

    Final changes, more formatting

--- src/DecimalAge.tsx
+++ src/DecimalAge.tsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { convertDate } from './util/convert';
+import React, { useState, useEffect } from "react";
+import { convertDate } from "./util/convert";
 
 type Props = {
-  date: string
-}
+  date: string;
+};
 
 export const DecimalAge: React.FC<Props> = ({ date }) => {
   //States
-  let [ageDisplay, setAgeDisplay] = useState('');
+  let [ageDisplay, setAgeDisplay] = useState("");
   let [parsedDate, setParsedDate] = useState({
-    years: '',
-    months: '',
-    days: ''
+    years: "",
+    months: "",
+    days: "",
   });
   //Make date into unix millisecond timestamp
   let unix_timestamp = Date.parse(date);
@@ -19,25 +19,32 @@ export const DecimalAge: React.FC<Props> = ({ date }) => {
     let mounted = true;
     setInterval(() => {
       //Simplified: current time unix - input time unix / (convert unix timestamp to years)
-      let time = (new Date().getTime() - new Date(unix_timestamp).getTime()) / (1000 * 60 * 60 * 24 * 365.25);
-      let decimalYear = time.toString().substring(0, 12)
+      let time =
+        (new Date().getTime() - new Date(unix_timestamp).getTime()) /
+        (1000 * 60 * 60 * 24 * 365.25);
+      let decimalYear = time.toString().substring(0, 12);
       //Packing data into states, only perform is component is mounted
-      if (mounted) { 
+      if (mounted) {
         let convertedDate = convertDate(Number(decimalYear));
         setAgeDisplay(decimalYear);
         setParsedDate({
           years: convertedDate.years.toString(),
           months: convertedDate.months.toString(),
-          days: convertedDate.days.toString()
+          days: convertedDate.days.toString(),
         });
-      };
+      }
     }, 50);
-    return () => { mounted = false };
+    return () => {
+      mounted = false;
+    };
   });
-	return (
-		<div className="decimalAge">
+  return (
+    <div className="decimalAge">
       <h1>Age: {ageDisplay}</h1>
-      <h2>Which Is Approximately: {parsedDate.years} years, {parsedDate.months} months, and {parsedDate.days} days</h2>
+      <h2>
+        Which Is Approximately: {parsedDate.years} years, {parsedDate.months}{" "}
+        months, and {parsedDate.days} days
+      </h2>
     </div>
-	);
+  );
 };
